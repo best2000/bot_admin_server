7a83bb78fa8416c999245db4019d551cfcaaa3da
best2000 <silentman@hotmail.co.th>
2022-07-26 00:53:25 +0700

    -

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from configparser import ConfigParser
+import json
+import sqlite3
 
 # uvicorn main:app --reload
 # api docs => http://127.0.0.1:8000/docs
@@ -13,16 +16,48 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def root():
-    return {"Hello": "World"}
+@app.get("/dashboard/{id}")
+async def dashboard(req: Request, id: str):
+    return templates.TemplateResponse("template.html", {"request": req, "id": id})
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: int = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/config/{id}")
+async def update_config(up_config: dict, id: str):
+    config = ConfigParser()
+    config.read("./stash/config.ini")
+    # check config key exist
+    # update config value
+    # config.set('SETTINGS', 'value', '15')
+    # with open('settings.ini', 'w') as configfile:
+    #     config.write(configfile)
+    return config._sections
 
 
-@app.get("/template/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("template.html", {"request": request, "id": id})
+@app.get("/status/{id}")
+async def get_status(req: Request, id: str):
+    data = {}
+    # instance
+    with open('./stash/instance.json') as file:
+        data['instance'] = json.load(file)
+    # config
+    config = ConfigParser()
+    config.read("./stash/config.ini")
+    data['config'] = config._sections
+    # #runtime logs
+    # with open('./stash/main.log') as file:
+    #     lines = file.readlines()
+    #     data['logs'] = [line.rstrip() for line in lines]
+    return data
+
+
+@app.get("/trade-logs/{id}")
+async def get_trade_logs(id: str, limit: int = 4):
+    conn = sqlite3.connect("./stash/log.db")
+    c = conn.cursor()
+    c.execute(
+        "SELECT * FROM trade_logs ORDER BY datetime DESC LIMIT {}".format(limit))
+    data = c.fetchall()
+    data.reverse()  # old...new
+    conn.commit()
+    conn.close()
+    return data
